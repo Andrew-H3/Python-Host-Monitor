4eaa36d0dfbe9d26aeb4e032e80550e75058a98a
Andrew <26437357+Andrew-H3@users.noreply.github.com>
2019-11-10 18:25:40 -0500

    Made config parse function

--- checkup.py
+++ checkup.py
@@ -3,37 +3,43 @@ import os
 # debug level: 1 = some, 2 = more
 debug = 1
 
-with open("checkup.conf", "r") as f:
-    i = 1
-    for line in f:
-        if line[0] == '#' or line[0] == ' ' or line[0] == '\n':
-            pass
-        else:
-            if debug >= 2:
-                print("parsing",line,end='')
-            args = line.strip().split(" ")
-            if debug >= 2:
-                print("args",args)
-            if args[0] == "Interval":
-                if debug >= 1:
-                    print("Setting Interval to",args[1])
-                Interval = int(args[1])
-            if args[0] == "Notify":
-                if debug >= 1:
-                    print("Will notify",args[1])
-                Notify = args[1]
-            if args[0] == "Host":
+# parse config file for parameters
+def parseConfigFile():
+    global Host
+    global Interval
+    global Notify
+    global Address
+    with open("checkup.conf", "r") as f:
+        for line in f:
+            if line[0] == '#' or line[0] == ' ' or line[0] == '\n':
+                pass
+            else:
                 if debug >= 2:
-                    print("Adding host",args[1])
-                parts = args[1].split("@")
-                if debug >= 1:
-                    print("Adding host",parts[0],"located at",parts[1])
-                Host = parts[0]
-                Address = parts[1]
+                    print("parsing",line,end='')
+                args = line.strip().split(" ")
+                if debug >= 2:
+                    print("args",args)
+                if args[0] == "Interval":
+                    if debug >= 1:
+                        print("Setting Interval to",args[1])
+                    Interval = int(args[1])
+                if args[0] == "Notify":
+                    if debug >= 1:
+                        print("Will notify",args[1])
+                    Notify = args[1]
+                if args[0] == "Host":
+                    if debug >= 2:
+                        print("Adding host",args[1])
+                    parts = args[1].split("@")
+                    if debug >= 1:
+                        print("Adding host",parts[0],"located at",parts[1])
+                    Host = parts[0]
+                    Address = parts[1]
 
 
+def StartUp():
+    print("Starting Up....")
+    print("Parsing config file....")
+    parseConfigFile()
 
-        #print("line",i,":",line,end='')
-        #i = i+1
-    
-
+StartUp()
