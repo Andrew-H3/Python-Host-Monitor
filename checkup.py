6b2354b3230e8c36f49f997d30de21ebb8a1d4e6
Andrew <26437357+Andrew-H3@users.noreply.github.com>
2019-11-10 17:48:11 -0500

    Script can read lines from conf file

--- checkup.py
+++ checkup.py
@@ -1,3 +1,13 @@
 import os
 
-print("hello")
+with open("checkup.conf", "r") as f:
+    i = 1
+    for line in f:
+        if line[0] == '#' or line[0] == ' ' or line[0] == '\n':
+            pass
+        else:
+            print(line,end='')
+        #print("line",i,":",line,end='')
+        #i = i+1
+    
+
